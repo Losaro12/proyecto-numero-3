56fa06368943497dff7b136ad1e883655541f71b
Losaro12 <142420514+Losaro12@users.noreply.github.com>
2023-12-12 17:21:07 -0300

    implementacion de menu y agregado de codigo al repositorio

---
+++ proyecto3.cpp
@@ -0,0 +1,57 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <random>
+#include <chrono>
+
+
+using namespace std;
+
+
+int main(){
+int num;
+
+cout << "bienvenido al programa\n ";
+cout << " \n";
+cout << "\n";
+cout << " \n";
+cout << "Salir del programa (4)\n";
+
+cout << "ingrese una de las opciones\n";
+cin >> num;
+
+
+do{
+switch (num)
+{
+case  1:
+    /* code */
+    break;
+case 2:
+
+    break;
+
+case 3:
+
+    break;
+
+case 4:
+    break;
+default:
+    break;
+}
+
+}while(num != 4);
+
+
+
+
+
+return 0;
+}
